f71bff0e7d4d3058ef24f9786e9bebbc73fc5175
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-05-05 19:31:27 +0600

    array sort asc and desc

--- mod-13.c
+++ mod-13.c
@@ -29,6 +29,94 @@
 // }
 
 // ! sum of two value answer is x
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[100000];
+//     for (int i = 0; i < n; i++)
+//     {
+//         /* code */
+//         scanf("%d", &arr[i]);
+//     }
+
+//     int x;
+//     scanf("%d", &x);
+//     int value1;
+//     int value2;
+//     int flag = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i + 1; j <= n; j++)
+//         {
+//             /* code */
+//             if (arr[i] + arr[j] == x)
+//             {
+//                 /* code */
+//                 value1 = arr[i];
+//                 value2 = arr[j];
+//                 flag = 1;
+//                 printf("Yes\n");
+//             }
+//         }
+//     }
+
+//     if (flag == 0)
+//     {
+//         printf("No\n");
+//     }
+//     else
+//     {
+
+//         printf("%d %d \n", value1, value2);
+//     }
+
+//     return 0;
+// }
+
+
+//! array sort asc 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[100000];
+//     for (int i = 0; i < n; i++)
+//     {
+//         /* code */
+//         scanf("%d", &arr[i]);
+//     }
+
+//     for (int i = 0; i <=n; i++)
+//     {
+//         /* code */
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (arr[i] > arr[j])
+//             {
+//                 /* code */
+//             int value1 = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = value1;
+//             }
+//         }
+//     }
+
+//     for (int k = 0; k < n; k++)
+//     {
+//         /* code */
+//         printf("%d ", arr[k]);
+//     }
+
+//     return 0;
+// }
+
+
+// !array sort desc 
 #include <stdio.h>
 
 int main()
@@ -42,35 +130,25 @@ int main()
         scanf("%d", &arr[i]);
     }
 
-    int x;
-    scanf("%d", &x);
-    int value1;
-    int value2;
-    int flag = 0;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <=n; i++)
     {
-        for (int j = i + 1; j <= n; j++)
+        /* code */
+        for (int j = i + 1; j < n; j++)
         {
-            /* code */
-            if (arr[i] + arr[j] == x)
+            if (arr[i] < arr[j])
             {
                 /* code */
-                value1 = arr[i];
-                value2 = arr[j];
-                flag = 1;
-                printf("Yes\n");
+            int value1 = arr[i];
+            arr[i] = arr[j];
+            arr[j] = value1;
             }
         }
     }
 
-    if (flag == 0)
+    for (int k = 0; k < n; k++)
     {
-        printf("No\n");
-    }
-    else
-    {
-
-        printf("%d %d \n", value1, value2);
+        /* code */
+        printf("%d ", arr[k]);
     }
 
     return 0;
