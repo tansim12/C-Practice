2dae8768c04614d21bf0c652e47046dfc7e4eb5e
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-05-05 15:32:14 +0600

    sum of two value answer is x

--- mod-13.c
+++ mod-13.c
@@ -1,29 +1,64 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n ;
+//     scanf("%d",&n);
+//     int value = 1;
+//     int space = n-1;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int s = 1; s <=space; s++)
+//         {
+//             /* code */
+//             printf(" ");
+//         }
+//         for (int j = 1; j <= value; j++)
+//         {
+//             /* code */
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//         value++;
+//         space--;
+
+//     }
+
+//     return 0;
+// }
+
+
+// ! sum of two value answer is x 
 #include <stdio.h>
 
 int main()
 {
-    int n ; 
-    scanf("%d",&n);
-    int star = 1;
-    int space = n-1;
-
+    int n;
+    scanf("%d", &n);
+    int arr[100000];
     for (int i = 0; i < n; i++)
     {
-        for (int s = 1; s <=space; s++)
-        {
-            /* code */
-            printf(" ");
-        }
-        for (int j = 1; j <= star; j++)
-        {
-            /* code */
-            printf("*");
-        }
-        printf("\n");
-        star+=2;
-        space--;
-        
+        /* code */
+        scanf("%d", &arr[i]);
     }
-    
+
+    int value1;
+    int value2;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            /* code */
+            if (arr[i] + arr[j] == 8)
+            {
+                /* code */
+                value1 = arr[j];
+                value2 = arr[i];
+            }
+        }
+    }
+    printf("%d %d",value1,value2);
+
     return 0;
 }
