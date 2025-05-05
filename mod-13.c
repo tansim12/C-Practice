5f56781a4502c40cc10e66cfe26db177ecb8a973
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2025-05-05 20:11:29 +0600

    reverse pyramid print

--- mod-13.c
+++ mod-13.c
@@ -117,39 +117,111 @@
 
 
 // !array sort desc 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[100000];
+//     for (int i = 0; i < n; i++)
+//     {
+//         /* code */
+//         scanf("%d", &arr[i]);
+//     }
+
+//     for (int i = 0; i <=n; i++)
+//     {
+//         /* code */
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (arr[i] < arr[j])
+//             {
+//                 /* code */
+//             int value1 = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = value1;
+//             }
+//         }
+//     }
+
+//     for (int k = 0; k < n; k++)
+//     {
+//         /* code */
+//         printf("%d ", arr[k]);
+//     }
+
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d",&n);
+//     int star = 1;
+//     int space = n-1;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         for (int s = 1; s <=space ; s++)
+//         {
+//             /* code */
+//             printf(" ");
+//         }
+        
+//         /* code */
+//         for (int j = 1; j <=star; j++)
+//         {
+//             /* code */
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//         star++;
+//         space--;
+
+        
+//     }
+    
+//     return 0;
+// }
+
+
+// ! reverse pyramid print 
+// *****
+// ****
+//  ***
+//   **
+//    *
 #include <stdio.h>
 
 int main()
 {
     int n;
-    scanf("%d", &n);
-    int arr[100000];
-    for (int i = 0; i < n; i++)
+    scanf("%d",&n);
+    int star = n;
+    int space = 0;
+    for (int i = n; i >=1; i--)
     {
-        /* code */
-        scanf("%d", &arr[i]);
-    }
-
-    for (int i = 0; i <=n; i++)
-    {
-        /* code */
-        for (int j = i + 1; j < n; j++)
+        for (int s = 1; s <=space ; s++)
         {
-            if (arr[i] < arr[j])
-            {
-                /* code */
-            int value1 = arr[i];
-            arr[i] = arr[j];
-            arr[j] = value1;
-            }
+            /* code */
+            printf(" ");
         }
-    }
-
-    for (int k = 0; k < n; k++)
-    {
+        
         /* code */
-        printf("%d ", arr[k]);
-    }
+        for (int j = star; j >=1; j--)
+        {
+            /* code */
+            printf("*");
+        }
+        printf("\n");
+        star--;
+        space++;
 
+        
+    }
+    
     return 0;
 }
