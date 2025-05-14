c021eee05f8dbdc3d7db88271da946d365eead09
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-05-14 15:27:30 +0600

    2d array print

--- twoDArray.c
+++ twoDArray.c
@@ -17,6 +17,16 @@ int main()
         }
     }
 
-    printf("%d", arr[1][0]);
+        for (int i = 0; i < r; i++)
+    {
+
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    
     return 0;
 }
